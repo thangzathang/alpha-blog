c6bad2ab2efdb237e3ba01bb9a3aa1b1f054dc96
thangzathang <thangzathang@gmail.com>
2021-06-08 12:41:18 +1000

    Signup button at home and other minor dynamic layouts done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    
+    def create
+        @user = User.new(user_params)
+
+        if @user.save 
+            flash[:notice] = "Welcome #{@user.username.split.first().capitalize}!, you have sucessfully signed up to Alpha Blogs"
+            redirect_to articles_path
+        else
+            render 'new' 
+        end
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :email, :password)
+    end
 
 end
