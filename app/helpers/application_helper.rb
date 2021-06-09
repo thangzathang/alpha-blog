d3613803674defb0ba877a82a3e2828a96d6c970
thangzathang <thangzathang@gmail.com>
2021-06-09 11:14:49 +1000

    Added Gravatar helper, show page for individual profile done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+    def gravatar_for(user, options = { size: 80 })
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+
+        size = options[:size]
+
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+
+        # The Class is from bootstrap - works because this is an <img> tag!
+        image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block") 
+    end
+
 end
